43a0abd7bc6220517f257d6d226a3cf635a6d0d6
quan dang <quan1612t@icloud.com>
2025-03-16 00:23:48 -0400

    Initial React app commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Korean Photobooth by Mi Pham and Quan Dang :))
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
